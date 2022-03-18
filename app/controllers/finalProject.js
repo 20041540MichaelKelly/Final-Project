d96f1e10c1cc7845739dad64c0af84a9c23cb83b
Michael <20041540@mail.wit.ie>
2022-03-18 12:55:13 +0000

    Have added a login page, this is to ensure only users I want to see my content will access it

--- app/controllers/finalProject.js
+++ app/controllers/finalProject.js
@@ -1,12 +1,22 @@
 const FinalProject = {
   index: {
     handler: function (request, h) {
-      return h.file("./app/views/main.html");
+      return h.view("main", { title: "Welcome to My final project" });
     },
   },
   login: {
     handler: function (request, h) {
-      return h.file("./app/views/login.html");
+      return h.view("login", { title: "Login to Project Directory" });
+    },
+  },
+  home: {
+    handler: function (request, h) {
+      return h.view("home", { title: "Make a Donation" });
+    },
+  },
+  logout: {
+    handler: function (request, h) {
+      return h.redirect("/");
     },
   },
 };
--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const FinalProject = require("./app/controllers/finalProject");
 module.exports = [
   { method: "GET", path: "/", config: FinalProject.index },
   { method: "GET", path: "/login", config: FinalProject.login },
+  { method: "GET", path: "/home", config: FinalProject.home },
+  { method: "GET", path: "/logout", config: FinalProject.logout },
+
   {
     method: "GET",
     path: "/{param*}",
